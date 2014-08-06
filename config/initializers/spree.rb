2ebfa322c02b6667b9fad282a882b70f70df53a1
Vladimir Mikhailov <vladimir.mikhailov@flatstack.com>
2014-08-06 12:47:13 +0400

    Setup spree for rails base app

---
+++ config/initializers/spree.rb
@@ -0,0 +1,26 @@
+# Configure Spree Preferences
+#
+# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
+#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
+#
+# In order to initialize a setting do:
+# config.setting_name = 'new value'
+Spree.config do |config|
+  # Example:
+  # Uncomment to stop tracking inventory levels in the application
+  # config.track_inventory_levels = false
+end
+
+Spree.user_class = 'Spree::LegacyUser'
+
+Rails.application.config.to_prepare do |g|
+  # Load application's model / class decorators
+  Dir.glob(File.join(File.dirname(__FILE__), '../app/**/*_decorator*.rb')) do |c|
+    Rails.configuration.cache_classes ? require(c) : load(c)
+  end
+
+  # Load application's view overrides
+  Dir.glob(File.join(File.dirname(__FILE__), '../app/overrides/*.rb')) do |c|
+    Rails.configuration.cache_classes ? require(c) : load(c)
+  end
+end
